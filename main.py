fc42566b1ed34a84d6f91c66c76aace47a119101
AmandineLabaeye <amandine.labaeye@orange.fr>
2020-11-25 11:13:32 +0100

    Story 2 effectué et fonctionnel

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import cv2
 img = cv2.imread("imgs/Logo_Tirna.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imwrite("outpout/Logo_Tirna_Gray.jpg", img)
+
+# Story 2
+img = cv2.imread("imgs/Logo_Tirna.jpg")
+img = cv2.blur(img, (5, 5))
+cv2.imwrite("outpout/Logo_Tirna_Blur.jpg", img)
