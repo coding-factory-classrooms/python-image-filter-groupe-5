6664bb073fee86c514d29fdf77d0f813df63eae7
AmandineLabaeye <amandine.labaeye@orange.fr>
2020-11-25 10:00:33 +0100

    Ajout des dossiers entrée et sortie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 
-#Story1
-img = cv2.imread("Logo_Tirna.jpg")
+# Story1
+img = cv2.imread("imgs/Logo_Tirna.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("tot.jpg", img)
+cv2.imwrite("outpout/Logo_Tirna_Gray.jpg", img)
