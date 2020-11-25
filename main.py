d736d351c2b500312bf0b07e8ce44c8253f5f435
AmandineLabaeye <amandine.labaeye@orange.fr>
2020-11-25 11:17:55 +0100

    Story 3 effectuée et finie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Story1
 img = cv2.imread("imgs/Logo_Tirna.jpg")
@@ -9,3 +10,9 @@ cv2.imwrite("outpout/Logo_Tirna_Gray.jpg", img)
 img = cv2.imread("imgs/Logo_Tirna.jpg")
 img = cv2.blur(img, (5, 5))
 cv2.imwrite("outpout/Logo_Tirna_Blur.jpg", img)
+
+# Story 3
+img = cv2.imread("imgs/Logo_Tirna.jpg")
+kernel = np.ones((5, 5), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+cv2.imwrite("outpout/Logo_Tirna_Dilate.jpg", img)
