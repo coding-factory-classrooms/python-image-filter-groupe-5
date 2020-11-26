d4b7a2568302741e687aedf4d4a93d08a9a3e278
AmandineLabaeye <amandine.labaeye@orange.fr>
2020-11-26 20:37:10 +0100

    Commentaire et Docstring rajouté sur fichier story 13

--- ZeTeam.py
+++ ZeTeam.py
@@ -2,22 +2,29 @@ import cv2
 
 
 def TransformZeTeam(img):
+    """
+    Permet de transformer plusieurs images avec notre prénom des membres de l'équipe
+    :param img: dossier de l'image que l'on doit transformer
+    :return: le filtre zeTeam est appliqué sur l'image
+    """
 
-    # font
+    # font Type de police
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # org
+    # org Cordonnée X et Y
     orgA = (0, 25)
     orgK = (0, 50)
+
     # fontScale
     fontScale = 1
 
     # Blue color in BGR
     color = (255, 0, 0)
 
-    # Line thickness of 2 px
+    # Line thickness of 2 px Epaisseur du txt
     thickness = 2
 
+    # Type de ligne à utiliser
     line_type = cv2.LINE_AA
 
     img = cv2.putText(img, f"Amandine", orgA, font, fontScale, color, thickness, line_type)
